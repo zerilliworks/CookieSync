e451c7d39ec4a31c791327da423a6674e8b0cfb5
Armand Zerilli <armand@zerilliworks.net>
2013-10-29 22:38:24 -0400

    Added Navbar view partial

---
+++ app/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <a class="navbar-brand" href="/about">CookieSync</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav">
+            <li class="{{ if_page('mysaves*', 'active') }}"><a href="/mysaves">My Saves</a></li>
+            <li class="{{ if_page('options*', 'active') }}"><a href="/options">Options</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/logout">Log Out</a></li>
+    </div><!-- /.navbar-collapse -->
+</nav>
