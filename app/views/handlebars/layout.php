68c9564c19d67048f41669bf3cc9b0ce89633670
Armand Zerilli <armand@zerilliworks.net>
2014-04-06 10:02:12 -0400

    New empty layout files for Ember conversion

---
+++ app/views/handlebars/layout.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CookieSync</title>
+    <?php if(isset($_page_description)): ?>
+    <meta name="description" content="<?php echo $_page_description ?>">
+    <?php else: ?>
+    <meta name="description" content="CookieSync - keep your Cookie Clicker saves updated wherever you go">
+    <?php endif; ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/css/cookiesync.css"/>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Kavoon' rel='stylesheet' type='text/css'>
+    @yield('css')
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+
+<script type="text/x-handlebars">
+<div class="container" style="margin-bottom: 3em">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1 col-xs-12">
+            {{outlet}}
+        </div>
+    </div>
+</div>
+</script>
+
+@include('partials.footer')
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/slabText/2.3/jquery.slabtext.min.js"></script>
+
+<script type="text/javascript">
+    var _gauges = _gauges || [];
+    (function() {
+        var t   = document.createElement('script');
+        t.type  = 'text/javascript';
+        t.async = true;
+        t.id    = 'gauges-tracker';
+        t.setAttribute('data-site-id', '526c9764108d7b49f6000021');
+        t.src = '//secure.gaug.es/track.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(t, s);
+    })();
+
+    window.addEventListener('storage', function(e) {
+        if(e.key === 'cookiesync.pulse') {
+            localStorage.setItem(e.key, '');
+            window.location.reload(true);
+        }
+    });
+
+</script>
+@yield('footer-js')
+</body>
+</html>
---
+++ app/views/handlebars/mysaves.php
@@ -0,0 +1,47 @@
+@include('partials.navbar')
+<div class="jumbotron">
+    <div class="row">
+        <h1>{{ $gameCount }} {{ trans_choice('objects.game', $gameCount) }}</h1>
+    </div>
+</div>
+@include('partials.alerts')
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+    <th>Game Start Date</th>
+    <th>Saves</th>
+    <th>Cookies <span class="text-muted">(current / all-time)</span></th>
+    <th>Latest Save</th>
+    <th></th>
+    </thead>
+    <tbody>
+    <?php foreach($games as $game): ?>
+        <?php
+        $latestSave = $game->latestSave();
+        if(!$latestSave)
+        {
+            continue;
+        }
+        ?>
+        <tr>
+            <td>
+                {{ with(new Carbon\Carbon($game->date_started))->toFormattedDateString() }}
+            </td>
+            <td>
+                {{ $game->saves()->count() }}
+            </td>
+            <td>
+                <b>{{ prettyNumbers($latestSave->cookies()) }}</b> / <i class="text-muted">{{ prettyNumbers($latestSave->allTimeCookies()) }}</i>
+            </td>
+            <td>
+                {{ $latestSave->updated_at->diffForHumans() }}
+            </td>
+            <td>
+                <a href="/games/{{ $game->id }}" class="btn btn-block btn-primary btn-sm">Details...</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+{{ $games->links() }}
+@stop
