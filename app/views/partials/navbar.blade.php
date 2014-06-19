d152db41b13f71a9f8e261d7074fd39b94f4d61b
Armand Zerilli <armand@zerilliworks.net>
2014-06-18 20:09:48 -0400

    Fixed navbar collapsing on mobile and slimmed down CSS

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,15 +1,21 @@
-<nav class="navbar" role="navigation">
+<nav class="navbar navbar-default" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
         <a class="navbar-brand" href="/cookiesync" style="font-family: Kavoon, Helvetica, Arial, sans-serif">CookieSync</a>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <div class="collapse navbar-collapse navbar-ex1-collapse" id="main-navbar-collapse">
         <ul class="nav navbar-nav">
-            <li class="{{ if_page('mysaves*', 'active') }}"><a href="{{ action('SavesController@index') }}">My Saves</a></li>
-            <li class="{{ if_page('games*', 'active') }}"><a href="{{ action('GamesController@index') }}">My Games</a></li>
-            <li class="{{ if_page('options*', 'active') }}"><a href="{{ action('OptionsController@getIndex') }}">Options</a></li>
+            <li class="{{ if_page('*mysaves*', 'active') }}"><a href="{{ action('SavesController@index') }}">My Saves</a></li>
+            <li class="{{ if_page('*games*', 'active') }}"><a href="{{ action('GamesController@index') }}">My Games</a></li>
+            <li class="{{ if_page('*options*', 'active') }}"><a href="{{ action('OptionsController@getIndex') }}">Options</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
             @if(Auth::check())
@@ -17,5 +23,6 @@
             @else
             <li><a href="{{ route('login') }}">Log In</a></li>
             @endif
+        </ul>
     </div><!-- /.navbar-collapse -->
 </nav>
